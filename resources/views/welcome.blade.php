45258c966f9e744f88c8a0e6f8c428d55d38d0fe
youssef tadlaoui <usftadlaoui@gmail.com>
2023-05-06 13:39:30 +0100

    old

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
             @csrf
                <div class="mb-3"> 
                 <label for="description" class="foem-label"> isnert a description for the image</label>
-                <input type="text" class="form-control  @error("description") is-invalid @enderror mt-3" name="description" id="description" placeholder="Exemple A fox diving into the sea" autofocus  maxlength="1000">
+                <input type="text" class="form-control  @error("description") is-invalid @enderror mt-3" name="description" id="description" placeholder="Exemple A fox diving into the sea" autofocus  maxlength="1000" value="{{old('description')}}">
                 @error("description")
                 <div  
                 class="invalid-feedback">
@@ -21,9 +21,14 @@
                 <div class="mb-3">
                     <label for="text" class="foem-label ">select the size of the image</label> 
                     <select class="form-select  @error("size") is-invalid @enderror" name="size" id="size" >
-                        <option value="sm">small</option>
-                        <option value="md">medium</option>
-                        <option value="lg">large</option>
+                        <option value="sm" @if (old('size')==='sm') selected
+                        @endif>small</option>
+                        <option value="md" @if (old('size')==='md')
+                            selected
+                        @endif>medium</option>
+                        <option value="lg" @if (old('size')==='lg')
+                            selected
+                        @endif>large</option>
                     </select>
                     @error("size")
                 <div  
